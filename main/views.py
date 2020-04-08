8fe474cd18d786d9a974a1c870b34d1d8306e7ed
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-04-08 23:52:19 +0300

    Implement django template base.html
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = "index.html"
+    template_name = "news_page.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
---
+++ static/assets/js/main.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector('.scrolling-wrapper');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX);
+    slider.scrollLeft = scrollLeft - walk;
+    //console.log(walk);
+});
+
+
+$("div #main-manu").mouseover(function() { $(this).addClass("menu-bg border rounded"); });
+$("div #main-manu").mouseout(function() { $(this).removeClass("menu-bg border rounded"); });
