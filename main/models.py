0e10f41183cd6c0d2aff35780fe3c37c61c53726
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-04-10 12:52:59 +0300

    Create models(HashtagModel/PostsModel/PostFilesModel)
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/models.py
+++ main/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class HashtagModel(models.Model):
+    """Hashtag post model"""
+    name = models.CharField('Name hashtag', max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Hashtag'
+        verbose_name_plural = 'Hashtags'
+
+
+class PostsModel(models.Model):
+    """Posts user model"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user')
+    text = models.TextField('Text posts', blank=True, max_length=2000)
+    date_time_create = models.DateTimeField('Date and time created post', auto_now_add=True)
+    hashtag = models.ManyToManyField(HashtagModel, verbose_name='hashtag', related_name='hashtags')
+    likes_count = models.IntegerField('Likes count', default=0)
+    comments_count = models.IntegerField('Comments count', default=0)
+
+    def __str__(self):
+        return self.user.username, self.text
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+class PostFilesModel(models.Model):
+    """Post files models"""
+    post = models.ForeignKey(PostsModel, on_delete=models.CASCADE, verbose_name='post')
+    file = models.FileField('File', max_length=255, upload_to='posts/image/'+ str(User.id) + '/')
+    type = models.CharField('Type file', max_length=100)
+
+    def __str__(self):
+        return self.file, self.type
+
+    class Meta:
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
--- signup/models.py
+++ signup/models.py
@@ -8,9 +8,9 @@ class UserProfile(models.Model):
     """User profile model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField('Image user', upload_to='profile/', blank=True)
-    folowers = models.ManyToManyField(User, verbose_name="folower", related_name="folowers")
-    folowing = models.ManyToManyField(User, verbose_name="folow", related_name="folows")
-    phone_number = models.CharField("Phone number", max_length=20)
+    folowers = models.ManyToManyField(User, verbose_name='folower', related_name='folowers')
+    folowing = models.ManyToManyField(User, verbose_name='folow', related_name='folows')
+    phone_number = models.CharField('Phone number', max_length=20)
     is_verified_phone = models.BooleanField('Verified phone number', default=False)
     verification_uuid = models.UUIDField('Unique Verification email UUID', default=uuid.uuid4)
     is_verified_email = models.BooleanField('Verified email', default=False)
@@ -19,5 +19,5 @@ class UserProfile(models.Model):
         return self.user.username
 
     class Meta:
-        verbose_name = "User_Profile"
-        verbose_name_plural = "User_Profiles"
+        verbose_name = 'User_Profile'
+        verbose_name_plural = 'User_Profiles'
