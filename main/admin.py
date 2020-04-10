c7d7901b32d283df6cf18fcaca875b4aab773b3b
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-04-10 13:03:24 +0300

    Register models in Django Admin
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(HashtagModel)
+class HashtagModelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(PostsModel)
+class PostsModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'date_time_create', 'likes_count', 'comments_count')
+
+    PostFilesModel
+
+@admin.register(PostFilesModel)
+class PostFilesModelAdmin(admin.ModelAdmin):
+    list_display = ('post', 'file', 'type')
--- signup/admin.py
+++ signup/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'image', 'is_verified_phone', 'is_verified_email')
