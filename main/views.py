6db43f04326b2e5cddd2d0474f0550366431fab7
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-04-14 21:14:15 +0300

    Fix show user first files in posts
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/views.py
+++ main/views.py
@@ -125,7 +125,7 @@ class UserPageView(TemplateView):
         news_posts = PostsModel.objects.filter(user=user_obj).order_by('-date_time_create')
         queryset = PostFilesModel.objects.none()
         for post in news_posts:
-            queryset |= PostFilesModel.objects.filter(post_id=post.id)
+            queryset |= PostFilesModel.objects.filter(post_id=post.id)[:1]
         context['news_posts'] = news_posts
         context['files_list'] = queryset
         context['owner_page'] = user_obj
