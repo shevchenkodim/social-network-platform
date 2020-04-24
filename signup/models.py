eceeae4e190969bf21f909ad2fe17210b2efd76b
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-04-24 16:20:58 +0300

    Improve UserProfile model
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- signup/models.py
+++ signup/models.py
@@ -7,6 +7,11 @@ User = get_user_model()
 class UserProfile(models.Model):
     """User profile model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    work = models.CharField('Work', max_length=100)
+    studied = models.CharField('Studied', max_length=100)
+    current_city = models.CharField('Current City', max_length=100)
+    home_town = models.CharField('Home Town', max_length=100)
+    relationship = models.CharField('Relationship', max_length=100)
     image = models.ImageField('Image user', upload_to='profile/', blank=True)
     folowers = models.ManyToManyField(User, verbose_name='folower', related_name='folowers', blank=True)
     folowing = models.ManyToManyField(User, verbose_name='folow', related_name='folows', blank=True)
