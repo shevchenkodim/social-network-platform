ee47c5c79a10a3c429e170a28d34fe15aae78881
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-05-06 14:01:28 +0300

    Implement UserProfileForm in view
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import TemplateView
 from django.contrib.auth import get_user_model
 from PIL import Image, ImageDraw, ImageFilter
@@ -8,6 +8,8 @@ from django.http import JsonResponse
 from django.conf import settings
 from django.db.models import F
 from .forms import UserProfileForm
+from signup.models import UserProfile
+from django.contrib import messages
 from .models import *
 import uuid
 import os
@@ -22,9 +24,23 @@ class SettingsGeneralView(TemplateView):
         context = super().get_context_data(**kwargs)
         if not self.request.user.is_authenticated:
             raise PermissionDenied
-        context['form'] = UserProfileForm()
+        context['form'] = UserProfileForm(instance=UserProfile.objects.get(user=self.request.user))
         context['menu_action'] = 'settings'
         return context
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form_aut = UserProfileForm(data=request.POST, instance=UserProfile.objects.get(user=self.request.user))
+            if form_aut.is_valid():
+                form_aut.save()
+                messages.success(request, 'Your information has been successfully updated!')
+                return redirect(reverse('settings_general_view'))
+            else:
+                messages.error(request, 'Error! Please try again later!')
+                return redirect(reverse('settings_general_view'))
+        else:
+            response_data = {'_code' : 1, '_status' : 'no' }
+            return JsonResponse(response_data)
 
 
 class PostDetailView(TemplateView):
