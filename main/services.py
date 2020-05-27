c380c52ea2032fb3081b2e8f7f86d06bb3035083
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-05-27 09:54:41 +0300

    Fix upload and processing image on create post
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- main/services.py
+++ main/services.py
@@ -1,78 +1,83 @@
 from PIL import Image, ImageDraw, ImageFilter
 from django.conf import settings
 from .models import PostFilesModel
+from django.contrib import messages
+from django.shortcuts import get_list_or_404
 from moviepy.editor import *
 import os
 
-def generate_gif_for_video(file_id):
-    file = PostFilesModel.objects.get(id=file_id)
-    name = file.file.url
-    name = name.split("/")
-    full_name = name[-1]
-    name = full_name.split(".")
-    name_gif = name[0]
-    clip = (VideoFileClip(os.path.join(settings.BASE_DIR, 'media/posts/image', full_name)))
-    clip.subclip((15.0),(18.0))
-    clip.resize(0.5)
-    clip.write_gif(os.path.join(settings.BASE_DIR, 'media/posts/gif/') +  name_gif +'.gif')
 
-    # print(os.path.join(settings.BASE_DIR, 'media/posts/image', full_name))
-    # print(os.path.exists(os.path.join(settings.BASE_DIR, 'media/posts/image', full_name)))
-    # # gif = os.system('ffmpeg -ss 10 -t 15 -i ' + os.path.join(settings.BASE_DIR, 'media/posts/image', full_name) + ' -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ' + os.path.join(settings.BASE_DIR, 'media/posts/image') +  name_gif +'.gif')
-    file.video_gif =  name_gif +'.gif'
-    file.save()
-    return True
+def _processingImage(file):
+    fileName = str(file.file).split('/')[-1]
 
-def image_processing(picture, new_file_name):
+    with Image.open(file.file) as imageFile:
+        img = Image.new('RGB', (1080, 1080), 'black')
+        (width, height) = imageFile.size
+        try:
+            if width > height:
+                new_width  = 1080
+                new_height = int(new_width * height / width)
+                imageFile = imageFile.resize((new_width, new_height), Image.ANTIALIAS)
+                (width, height) = imageFile.size
+                new_height = int((1080 - height)/2)
+                img.paste(imageFile, (0, new_height))
+
+                #top
+                (left, upper, right, lower) = (0, new_height, new_width, new_height*2)
+                im_crop = img.crop((left, upper, right, lower))
+                blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
+                img.paste(blurred_image, (0, 0))
+
+                #botton
+                (left, upper, right, lower) = (0, 1080-new_height*2, new_width, 1080-new_height)
+                im_crop = img.crop((left, upper, right, lower))
+                blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
+                img.paste(blurred_image, (0, 1080-new_height))
+                img.save(settings.MEDIA_ROOT + '/' + fileName)
+                file.file = str(settings.MEDIA_ROOT + '/' + fileName)
+                file.save()
+            elif width < height:
+                new_height = 1080
+                new_width = int(new_height * width / height)
+                imageFile = imageFile.resize((new_width, new_height), Image.ANTIALIAS)
+                (width, height) = imageFile.size
+                new_width = int((1080 - width)/2)
+                img.paste(imageFile, (new_width, 0))
+
+                #left
+                (left, upper, right, lower) = (new_width, 0, new_width*2, 1080)
+                im_crop = img.crop((left, upper, right, lower))
+                blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
+                img.paste(blurred_image, (0, 0))
+
+                #right
+                (left, upper, right, lower) = (1080-new_width*2, 0, 1080-new_width, 1080)
+                im_crop = img.crop((left, upper, right, lower))
+                blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
+                img.paste(blurred_image, (1080-new_width, 0))
+                img.save(settings.MEDIA_ROOT + '/' + fileName)
+                file.file = str(settings.MEDIA_ROOT + '/' + fileName)
+                file.save()
+            elif width == height:
+                pass
+        except Exception as e:
+            print(e)
+
+
+def _processingVideo():
+    pass
+
+
+def processingImagesAndVideos(request, post_id):
+    files = get_list_or_404(PostFilesModel, post_id=post_id)
+    print(files)
     try:
-        img = Image.new('RGBA', (1080, 1080), 'black')
-        img_to = Image.open(picture)
-        (width, height) = img_to.size
-
-        if width > height:
-            new_width  = 1080
-            new_height = int(new_width * height / width)
-            img_to = img_to.resize((new_width, new_height), Image.ANTIALIAS)
-            (width, height) = img_to.size
-            new_height = int((1080 - height)/2)
-            img.paste(img_to, (0, new_height))
-
-            #top
-            (left, upper, right, lower) = (0, new_height, new_width, new_height*2)
-            im_crop = img.crop((left, upper, right, lower))
-            blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-            img.paste(blurred_image, (0, 0))
-
-            #botton
-            (left, upper, right, lower) = (0, 1080-new_height*2, new_width, 1080-new_height)
-            im_crop = img.crop((left, upper, right, lower))
-            blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-            img.paste(blurred_image, (0, 1080-new_height))
-
-            img.save(settings.MEDIA_ROOT + '/posts/image/' + str(new_file_name) + '.png')
-            return str(settings.MEDIA_ROOT + '/posts/image/' + str(new_file_name) + '.png')
-        else:
-            new_height = 1080
-            new_width = int(new_height * width / height)
-            img_to = img_to.resize((new_width, new_height), Image.ANTIALIAS)
-            (width, height) = img_to.size
-            new_width = int((1080 - width)/2)
-            img.paste(img_to, (new_width, 0))
-
-            #left
-            (left, upper, right, lower) = (new_width, 0, new_width*2, 1080)
-            im_crop = img.crop((left, upper, right, lower))
-            blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-            img.paste(blurred_image, (0, 0))
-
-            #right
-            (left, upper, right, lower) = (1080-new_width*2, 0, 1080-new_width, 1080)
-            im_crop = img.crop((left, upper, right, lower))
-            blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-            img.paste(blurred_image, (1080-new_width, 0))
-
-            img.save(settings.MEDIA_ROOT + '/posts/image/' + str(new_file_name) + '.png')
-            return str(settings.MEDIA_ROOT + '/posts/image/' + str(new_file_name) + '.png')
+        for file in files:
+            if file.type == 'image':
+                _processingImage(file)
+            elif file.type == 'video':
+                _processingVideo()
+        messages.success(request, 'Your successfully add new post! Please reload this page! :)')
     except Exception as e:
         print(e)
-        return False
+        messages.error(request, 'Error! Please try again later!')
--- main/views.py
+++ main/views.py
@@ -10,7 +10,7 @@ from django.db.models import F
 from django.contrib import messages
 from .models import Bookmarks, HashtagModel, PostsModel, PostFilesModel, CommentModel,\
 LikesModel
-from .services import generate_gif_for_video
+from .services import processingImagesAndVideos
 import uuid
 import os
 User = get_user_model()
@@ -129,7 +129,7 @@ def create_comment(request, pk):
 
 def create_new_post(request):
     """Create new user post"""
-    ALLOWED_TYPES_IMAGE = ['jpg', 'jpeg']
+    ALLOWED_TYPES_IMAGE = ['jpg', 'jpeg', 'png', 'gif']
     ALLOWED_TYPES_VIDEO = ['mp4', 'avi']
     if request.method == 'POST':
         if not request.user.is_authenticated:
@@ -154,60 +154,10 @@ def create_new_post(request):
                 extension = os.path.splitext(file.name)[1][1:].lower()
                 if extension in ALLOWED_TYPES_VIDEO:
                     video = PostFilesModel.objects.create(post=new_post, file=file, type='video', position=position)
-                    generate_gif_for_video(video.id)
                 if extension in ALLOWED_TYPES_IMAGE:
-                    new_post_file = PostFilesModel.objects.create(post=new_post, file=file, type='image', position=position)
-
-                    img = Image.new('RGB', (1080, 1080), 'black')
-                    img_to = Image.open(file)
-                    (width, height) = img_to.size
-
-                    if width > height:
-                        new_width  = 1080
-                        new_height = int(new_width * height / width)
-                        img_to = img_to.resize((new_width, new_height), Image.ANTIALIAS)
-                        (width, height) = img_to.size
-                        new_height = int((1080 - height)/2)
-                        img.paste(img_to, (0, new_height))
-
-                        #top
-                        (left, upper, right, lower) = (0, new_height, new_width, new_height*2)
-                        im_crop = img.crop((left, upper, right, lower))
-                        blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-                        img.paste(blurred_image, (0, 0))
-
-                        #botton
-                        (left, upper, right, lower) = (0, 1080-new_height*2, new_width, 1080-new_height)
-                        im_crop = img.crop((left, upper, right, lower))
-                        blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-                        img.paste(blurred_image, (0, 1080-new_height))
-                        img.save(settings.MEDIA_ROOT + '/' + new_post_file.file.name)
-                        new_post_file.file = str(settings.MEDIA_ROOT + '/' + new_post_file.file.name)
-                        new_post_file.save()
-                    else:
-                        new_height = 1080
-                        new_width = int(new_height * width / height)
-                        img_to = img_to.resize((new_width, new_height), Image.ANTIALIAS)
-                        (width, height) = img_to.size
-                        new_width = int((1080 - width)/2)
-                        img.paste(img_to, (new_width, 0))
-
-                        #left
-                        (left, upper, right, lower) = (new_width, 0, new_width*2, 1080)
-                        im_crop = img.crop((left, upper, right, lower))
-                        blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-                        img.paste(blurred_image, (0, 0))
-
-                        #right
-                        (left, upper, right, lower) = (1080-new_width*2, 0, 1080-new_width, 1080)
-                        im_crop = img.crop((left, upper, right, lower))
-                        blurred_image = im_crop.filter(ImageFilter.GaussianBlur(10))
-                        img.paste(blurred_image, (1080-new_width, 0))
-                        img.save(settings.MEDIA_ROOT + '/' + new_post_file.file.name)
-                        new_post_file.file = str(settings.MEDIA_ROOT + '/' + new_post_file.file.name)
-                        new_post_file.save()
+                    postFile = PostFilesModel.objects.create(post=new_post, file=file, type='image', position=position)
                 position += 1
-
+            processingImagesAndVideos(request, new_post.id)
             response_data = {'_code' : 0, '_status' : 'ok' }
             return JsonResponse(response_data)
     else:
