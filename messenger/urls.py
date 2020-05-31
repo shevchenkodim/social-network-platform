dae61c5c5d940368916f2955c43b67dab352881e
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-05-31 07:29:55 +0300

    Implement load messages in chat
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- messenger/urls.py
+++ messenger/urls.py
@@ -6,4 +6,5 @@ app_name = 'messenger'
 urlpatterns = [
     path('', MessengerView.as_view(), name='messenger'),
     path('chat/<uuid>/', ChatView.as_view(), name='chat'),
+    path('chat/load/messages/<uuid>', views.chat_load_messages, name='load_chat_messages')
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import UserChat, UserMessages, UsersInChat
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -30,9 +31,29 @@ class ChatView(TemplateView):
         user_in_chat = UsersInChat.objects.filter(user=self.request.user).values_list('chat_id_id', flat=True)
         chats = UserChat.objects.filter(id__in=user_in_chat)
         chat = UserChat.objects.get(chat_id=self.kwargs['uuid'])
-        messages = UserMessages.objects.filter(chat_id=chat).order_by('created_at')
+        messages = UserMessages.objects.filter(chat_id=chat).reverse()[::-1][:20][::-1]
         context['messages'] = messages
         context['chat_page'] = chat
         context['chats'] = chats
 
         return context
+
+
+def chat_load_messages(request, uuid):
+    if request.method == 'GET':
+        if not request.user.is_authenticated:
+            raise PermissionDenied
+        page = request.GET.get('page')
+        page = int(page)
+        if page in [0, 1, None]:
+            return HttpResponse('')
+        start = page - 1
+        user_in_chat = UsersInChat.objects.filter(user=request.user).values_list('chat_id_id', flat=True)
+        chats = UserChat.objects.filter(id__in=user_in_chat)
+        chat = UserChat.objects.get(chat_id=uuid)
+        messages = UserMessages.objects.filter(chat_id=chat).reverse()[::-1][start*20:page*20][::-1]
+        if not messages:
+            return HttpResponse('')
+        return render(request, 'load_chat_messages.html', {'messages': messages})
+    else:
+        return HttpResponse('')
