212eb79865e9bf5d18811d2dc8df96c524ad9269
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-05 07:35:27 +0300

    Create page settings language
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- settings/urls.py
+++ settings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SettingsGeneralView
+from .views import SettingsGeneralView, SettingsLanguageView
 from . import views
 
 app_name = 'settings'
 urlpatterns = [
     path('general', SettingsGeneralView.as_view(), name='settings_general_view'),
+    path('language', SettingsLanguageView.as_view(),
+         name='settings_language_view'),
 ]
--- settings/views.py
+++ settings/views.py
@@ -6,8 +6,10 @@ from signup.models import UserProfile
 from django.http import JsonResponse
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import PermissionDenied
 
 # Create your views here.
+
 
 class SettingsGeneralView(TemplateView):
     """Settings general page View"""
@@ -17,20 +19,46 @@ class SettingsGeneralView(TemplateView):
         context = super().get_context_data(**kwargs)
         if not self.request.user.is_authenticated:
             raise PermissionDenied
-        context['form'] = UserProfileForm(instance=UserProfile.objects.get(user=self.request.user))
+        context['form'] = UserProfileForm(
+            instance=UserProfile.objects.get(user=self.request.user))
         context['menu_action'] = 'settings'
         return context
 
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
-            form_aut = UserProfileForm(data=request.POST, instance=UserProfile.objects.get(user=self.request.user))
+            form_aut = UserProfileForm(
+                data=request.POST, instance=UserProfile.objects.get(user=self.request.user))
             if form_aut.is_valid():
                 form_aut.save()
-                messages.success(request, _('Your information has been successfully updated!'))
+                messages.success(request, _(
+                    'Your information has been successfully updated!'))
                 return redirect(reverse('settings:settings_general_view'))
             else:
                 messages.error(request, _('Error! Please try again later!'))
                 return redirect(reverse('settings:settings_general_view'))
         else:
-            response_data = {'_code' : 1, '_status' : 'no' }
+            response_data = {'_code': 1, '_status': 'no'}
             return JsonResponse(response_data)
+
+
+class SettingsLanguageView(TemplateView):
+    """Settings language of the region"""
+    template_name = "settings_language.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied
+        context['menu_action'] = 'settings'
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            language = request.POST.get('language')
+            print(language)
+            messages.success(request, _(
+                'Your language successfully updated!'))
+            return redirect(reverse('settings:settings_language_view'))
+        else:
+            messages.error(request, _('Error! Please try again later!'))
+            return redirect(reverse('settings:settings_language_view'))
